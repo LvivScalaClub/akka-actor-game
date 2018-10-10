e8d2c480aeb1d670d577d4deffe8a79adc5179f4
Viktor Molitskyi <molitskyi@gmail.com>
2018-10-10 11:24:16 +0300

    Added `double` logic

--- src/main/scala/io/lvivscalaclub/akka_actor_game/MessagingProtocol.scala
+++ src/main/scala/io/lvivscalaclub/akka_actor_game/MessagingProtocol.scala
@@ -1,5 +1,7 @@
 package io.lvivscalaclub.akka_actor_game
 
+import io.lvivscalaclub.akka_actor_game.models.Card.Card
+
 sealed trait MessagingProtocol
 
 case class NewGameRequest(name: String, userId: String) extends MessagingProtocol
@@ -16,3 +18,9 @@ case object PlayerAlreadyConnected extends Error
 
 case class RollRequest(userId: String) extends MessagingProtocol
 case class RollResponse(screen: Seq[Seq[Int]], win: Long) extends MessagingProtocol
+
+case object TakeWinRequest extends MessagingProtocol
+case object GoToDoubleRequest extends MessagingProtocol
+
+case class DoubleRequest(card: Card) extends MessagingProtocol
+case class DoubleResponse(win: Long) extends MessagingProtocol
